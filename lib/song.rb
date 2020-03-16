7cf2b42ae715f217ae40581500e33ac12d7581ce
Maria Connie Sanchez <msnchz04@gmail.com>
2020-03-16 01:14:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,12 +9,13 @@ class Song
   
   def self.new_by_filename(file)
     name = file.split(" - ")
-    new_song - Song.new(name[])
+    new_song - Song.new(name[1])
     
   end 
   
   def artist_name=(name)
-    song
+    new_artist = Artist.find_or_create_by_name(name)
+    sedlf.artist = new_artist
   end 
   
   def self.all
